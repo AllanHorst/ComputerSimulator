f5a975ac93fafed79ea39ba1789c35ce11635355
AllanHorst <allanhorstwillig@gmail.com>
2017-11-19 12:22:15 -0200

    Created tests for new, set_address and insert on Computer

--- Api/app/services/computer.rb
+++ Api/app/services/computer.rb
@@ -1,2 +1,22 @@
 class Computer
+  include ActiveModel::Model
+  attr_accessor :pointer, :stack_size, :instructions
+
+  def initialize(stack_size)
+    @stack_size = stack_size
+    @pointer = 0
+    @instructions = []
+  end
+
+  def set_address(address)
+    @pointer = address
+  end
+
+  def insert(command, value=nil)
+    @instructions << ({
+      command: command,
+      value: value
+    })
+  end
+
 end
--- Api/spec/services/computer_spec.rb
+++ Api/spec/services/computer_spec.rb
@@ -2,4 +2,52 @@ require 'rails_helper'
 
 RSpec.describe V1::ComputersController, type: :service do
 
+  let(:computer) {
+    Computer.new(20)
+  }
+
+  context '#new' do
+    it 'pointer must be 0' do
+      expect(computer.pointer).to eq(0)
+    end
+
+    it 'stack size must be 20' do
+      expect(computer.stack_size).to eq(20)
+    end
+
+    it 'instructions cannot be nil' do
+      expect(computer.instructions).to_not eq(nil)
+    end
+  end
+
+  context '#set_address' do
+    let(:new_address) {
+      10
+    }
+    let(:start_address) {
+      0
+    }
+
+    it 'set pointer' do
+      computer.set_address(new_address)
+      expect(computer.pointer).to eq(new_address)
+    end
+
+    it 'change pointer' do
+      computer.set_address(start_address)
+      expect(computer.pointer).to eq(start_address)
+    end
+
+  end
+
+  context '#insert' do
+
+    it 'insert new command into instructions' do
+      computer.insert('push', 10)
+      expect(computer.instructions.size).to eq(1)
+    end
+
+  end
+
+
 end
