bd0cc76cb65c499d1958b4f0a1e83b12ae6695aa
AllanHorst <allanhorstwillig@gmail.com>
2017-11-20 00:05:44 -0200

    Save item in computer stack

--- App/src/components/computer-simulator.vue
+++ App/src/components/computer-simulator.vue
@@ -4,12 +4,34 @@
     <p>Choose the size of your stack and generate a new computer</p>
 
     <div class="container">
-      <div class="input-field">
-        <input type="number" placeholder="Stack's Size" v-model="stackSize">
-        <button class="btn primary" v-on:click="createComputer()">Create Computer</button>
+      <input type="number" placeholder="Stack's Size" v-model="stackSize">
+      <button class="btn primary" v-on:click="createComputer()">Create Computer</button>
+
+      <div class="list" v-if="computerId != null">
+        <ul>
+
+          <li class="new">
+            <label for="instruction">Instruction</label>
+            <select id="instruction" v-model="stackItem.instruction">
+              <option v-bind:value="instruction" v-for="(instruction, index) in instructions" v-bind:key="index">
+                {{instruction.name}}
+              </option>
+            </select>
+            <input type="number"
+              placeholder="Param"
+              v-model="stackItem.arg"
+              v-bind:disabled="!stackItem.instruction || !stackItem.instruction.param">
+            <button class="btn primary" v-on:click="addToStack()">Create Computer</button>
+          </li>
+
+          <li v-for="(item, index) in stack.slice().reverse()" v-bind:key="index">
+            <span class="address">{{index}}</span>
+            <span class="instruction">{{item.instruction}}</span>
+            <span class="param">{{item.param}}</span>
+          </li>
+        </ul>
       </div>
     </div>
-
 
   </div>
 </template>
@@ -22,18 +44,65 @@ export default {
   data () {
     return {
       stackSize: '',
-      computerId: null
+      computerId: null,
+      stack: [],
+      stackItem: {},
+      instructions: [
+        {
+          name: 'PUSH',
+          param: true
+        },
+        {
+          name: 'MULT',
+          param: false
+        },
+        {
+          name: 'RET',
+          param: false
+        },
+        {
+          name: 'PRINT',
+          param: false
+        },
+        {
+          name: 'CALL',
+          param: true
+        },
+        {
+          name: 'STOP',
+          param: false
+        }]
     }
   },
   methods: {
     createComputer() {
       this.$http.post(URL, {stack: this.stackSize}).then(data => {
-        console.log(data)
         this.computerId = data.body.id
       })
+    },
+    addToStack() {
+      let obj = {
+        arg: this.stackItem.arg
+      }
+      this.$http.post(URL + `${this.computerId}/stack/insert/${this.stackItem.instruction.name}`, {arg: this.stackItem.arg}).then(data => {
+          this.stack = data.body.stack.map( item => {
+            if (!item) {
+              return {}
+            }
+            return JSON.parse(item)
+          })
+          this.stackItem = {}
+      })
+    }
+  },
+  filters: {
+    reverse (value) {
+      console.log('so loko')
+      return value.slice().reverse()
     }
   }
 }
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
