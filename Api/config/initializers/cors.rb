465b8b50bc40ef78e73d712e1f6c1e808bd8f644
AllanHorst <allanhorstwillig@gmail.com>
2017-11-20 01:46:52 -0200

    Allow app access

--- Api/config/initializers/cors.rb
+++ Api/config/initializers/cors.rb
@@ -14,7 +14,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
   allow do
-    origins 'computer-simulator-app.herokuapp.com/'
+    origins 'https://computer-simulator-app.herokuapp.com/'
 
     resource '*',
       headers: :any,
